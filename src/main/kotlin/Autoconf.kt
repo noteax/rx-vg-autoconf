0d2fc96ad9563d8e7ab9252d5611f940766a4804
aidar <aibariev@fix.ru>
2018-01-21 20:15:56 +0300

    Some additions

--- src/main/kotlin/Autoconf.kt
+++ src/main/kotlin/Autoconf.kt
@@ -1,46 +1,80 @@
+import mmarquee.automation.ElementNotFoundException
 import mmarquee.automation.UIAutomation
 import mmarquee.automation.controls.*
 import mmarquee.automation.controls.mouse.AutomationMouse
 import mmarquee.automation.uiautomation.ToggleState
 import java.util.regex.Pattern
 
+data class Configuration(val numberOfCards: Int,
+                         val configureHbcc: Boolean,
+                         val wattmanConf: WattmanConf?)
+
+data class WattmanConf(val gpuFreq: Int,
+                       val memFreq: Int,
+                       val tempTarget: Int,
+                       val powerLimit: Int)
+
 fun main(args: Array<String>) {
+    if (args.size != 3 && args.size != 2) {
+        println("Incorrent number of args. Required \"'numberOfCards' 'configureHbcc' 'gpuFreq:memFreq:templTarget:powerLimit'\"")
+        return
+    }
+
+    val wattmanConf = if (args.size > 2) {
+        val (gpuFreq, memFreq, tempTarget, powerLimit) = args[3].split(":").take(4).map(String::toInt)
+        WattmanConf(gpuFreq, memFreq, tempTarget, powerLimit)
+    } else null
+
     // args
     val applicationPath = args[0]
-    val (numberOfCards, gpuFreq, memFreq, tempTarget, powerLimit) = args.drop(1).take(5).map(String::toInt)
+    val conf: Configuration = Configuration(
+            args[1].toInt(),
+            args[2].toBoolean(),
+            wattmanConf)
 
     println(applicationPath)
 
     val automation = UIAutomation.getInstance()
     automation.launch(applicationPath).waitForInputIdle()
 
-    for (i in 0 until numberOfCards) {
-        val currentState = configureHbcc(automation, i)
-        if (!currentState) {
-            configureHbcc(automation, i)
+    if (conf.configureHbcc) {
+        for (i in 0 until conf.numberOfCards) {
+            val currentState = configureHbcc(automation, i, applicationPath)
+            if (!currentState) {
+                configureHbcc(automation, i, applicationPath)
+            }
+            println("HBCC configuration $i complete")
         }
-        println("HBCC configuration $i complete")
     }
 
-    val window = openGlobalSettings(automation)
-    for (i in 0 until numberOfCards) {
-        configureWattman(window, i, numberOfCards,
-                gpuFreq, memFreq, tempTarget, powerLimit)
+    if (conf.wattmanConf != null) {
+        val window = openGlobalSettings(automation, applicationPath)
+        for (i in 0 until conf.numberOfCards) {
+            configureWattman(window, i, conf.numberOfCards, conf.wattmanConf)
+        }
     }
-
 }
 
-fun getWindow(automation: UIAutomation): AutomationWindow {
-    val windowName = "RADEON SETTINGS RADEON SETTINGS"
-    return automation.getDesktopWindow(windowName, 180)
+fun getWindow(automation: UIAutomation, applicationPath: String): AutomationWindow {
+    try {
+        val windowName = "RADEON SETTINGS RADEON SETTINGS"
+        return automation.getDesktopWindow(windowName, 180)
+    } catch (e: ElementNotFoundException) {
+        // close all radeon apps and open again
+        Runtime.getRuntime().exec("taskkill /F /IM RadeonSettings.exe")
+        automation.launch(applicationPath).waitForInputIdle()
+
+        val windowName = "RADEON SETTINGS RADEON SETTINGS"
+        return automation.getDesktopWindow(windowName, 180)
+    }
 }
 
-fun openGlobalSettings(automation: UIAutomation): AutomationWindow {
+fun openGlobalSettings(automation: UIAutomation, applicationPath: String): AutomationWindow {
     val maximizeButton = "maximize/restore button"
     val gamesButton = "main app list - Gaming_\$_Gaming"
     val globalButton = "Game 0_\$_Global Settings"
 
-    val window = getWindow(automation)
+    val window = getWindow(automation, applicationPath)
     window.focus()
 
     window.getButton(maximizeButton).click()
@@ -50,13 +84,13 @@ fun openGlobalSettings(automation: UIAutomation): AutomationWindow {
     return window
 }
 
-fun configureHbcc(automation: UIAutomation, index: Int): Boolean {
+fun configureHbcc(automation: UIAutomation, index: Int, applicationPath: String): Boolean {
     val hbccButton = "Game Manager - Global Settings page button - ${index}_\$_Global Graphics\n(Radeon RX Vega)"
     val hbccCheckBoxPattern = "virtualmemory .* ComboBoxToggle.*"
     val hbccApplyButton = "virtual memory bar - apply button"
     val hbccApplyConfirmButton = "virtual memory confirm dialog - dialog button - Confirm"
 
-    val window = openGlobalSettings(automation)
+    val window = openGlobalSettings(automation, applicationPath)
 
     window.getButton(hbccButton).click()
     Thread.sleep(3000)
@@ -78,11 +112,7 @@ fun configureHbcc(automation: UIAutomation, index: Int): Boolean {
     return !currentStatus
 }
 
-fun configureWattman(window: AutomationWindow, index: Int, cardsCount: Int,
-                     gpuFreq: Int,
-                     memFreq: Int,
-                     tempTarget: Int,
-                     powerLimit: Int) {
+fun configureWattman(window: AutomationWindow, index: Int, cardsCount: Int, conf: WattmanConf) {
     val wattmanButton = "Game Manager - Global Settings page button - ${cardsCount + index}_\$_Global WattMan\n(Radeon RX Vega)"
     val scrollBar = "game manager overdrive scrollbar"
     val gpuFreqSlider = "GPU Frequency Slider_.*"
@@ -103,21 +133,21 @@ fun configureWattman(window: AutomationWindow, index: Int, cardsCount: Int,
     val scrollBarControl = window.getControlByName(scrollBar)
     for (i in 0..13) scrollBarControl.invoke()
 
-    setSliderVal(findElement(window, gpuFreqSlider) as AutomationSlider, gpuFreq, cpuFreqRange)
-    println("Configure gpuFreq to $gpuFreq")
+    setSliderVal(findElement(window, gpuFreqSlider) as AutomationSlider, conf.gpuFreq, cpuFreqRange)
+    println("Configure gpuFreq to ${conf.gpuFreq}")
 
-    setSliderVal(findElement(window, memoryFreqSlider) as AutomationSlider, memFreq, memFreqRange)
-    println("Configure memFreq to $memFreq")
+    setSliderVal(findElement(window, memoryFreqSlider) as AutomationSlider, conf.memFreq, memFreqRange)
+    println("Configure memFreq to ${conf.memFreq}")
 
     val tempToggleV = findElement(window, tempToggle) as AutomationCheckBox
     if (tempToggleV.toggleState != ToggleState.On) tempToggleV.toggle()
     println("Set temp to manual")
 
-    setSliderVal(findElement(window, tempTargetSlider) as AutomationSlider, tempTarget, tempTargetRange, false)
-    println("Set tempTarget to $tempTarget")
+    setSliderVal(findElement(window, tempTargetSlider) as AutomationSlider, conf.tempTarget, tempTargetRange, false)
+    println("Set tempTarget to ${conf.tempTarget}")
 
-    setSliderVal(findElement(window, powerLimitSlider) as AutomationSlider, powerLimit, powerLimitRange)
-    println("Set powerLimit to $powerLimit")
+    setSliderVal(findElement(window, powerLimitSlider) as AutomationSlider, conf.powerLimit, powerLimitRange)
+    println("Set powerLimit to ${conf.powerLimit}")
 
     try {
         window.getButton(applyButton).click()
